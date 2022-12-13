04c2a6086a22d55bfd97fc2f60e15fac6995fae5
unknown <qintharalmaliki@gmail.com>
2022-12-13 13:41:30 +0700

    beres

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={"https://cf.shopee.co.id/file/fdf7189ef10b5b32b9d0fd42b65bba9d"} className="App-logo" alt="logo" />
+        <p>
+          BEST CLUB IN THE WORLD
+        </p>
+        <a
+          className="App-link"
+          href="https://www.juventus.com/id/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
